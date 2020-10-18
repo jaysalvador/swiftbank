abe3a292c0f2697937bf34f65eb06ce6fa6e055e
Jay Andrae Salvador <jayandrae.salvador@optus.com.au>
2020-10-18 16:44:32 +1100

    Initial Commit

---
+++ Before Pay/Before_PayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Before_PayApp.swift
+//  Before Pay
+//
+//  Created by Jay Andrae Salvador on 18/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Before_PayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Before Pay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Before Pay
+//
+//  Created by Jay Andrae Salvador on 18/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
